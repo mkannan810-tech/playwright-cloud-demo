868d5ba5704c4e52d517d251319e6bd70a367354
mkannan810-tech <mkannan810@gmail.com>
2025-08-14 00:48:40 +0530

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,22 +1,13 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page
 
-def test_invalid_login():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context(record_video_dir="videos/")
-        page = context.new_page()
-        context.tracing.start(screenshots=True, snapshots=True, sources=True)
+def test_invalid_login(page: Page):
+    page.goto("https://the-internet.herokuapp.com/login")
+    page.fill("#username", "tomsmith")
+    page.fill("#password", "wrongpassword")
+    page.click("button[type='submit']")
 
-        page.goto("https://the-internet.herokuapp.com/login")
-        page.fill("#username", "tomsmith")
-        page.fill("#password", "wrongpassword")
-        page.click("button[type='submit']")
+    # Verify error message
+    assert "Your password is invalid!" in page.text_content("#flash")
 
-        error_message = page.text_content("#flash")
-
-        context.tracing.stop(path="trace_login.zip")
-        browser.close()
-
-        assert "Your password is invalid!" in error_message
-   # Wait 3 seconds so video shows the result
+    # Wait 3 seconds so video shows the result
     page.wait_for_timeout(3000)
